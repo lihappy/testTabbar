159b15d981fce1b82c7ccedd761993a972b9be01
Li, Haibo <haibol@amazon.com>
2017-02-15 16:19:07 +0800

    init

--- testTab/ViewController.swift
+++ testTab/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func back(_ sender: Any) {
+        self .dismiss(animated: true, completion: nil);
+    }
 
 }
 
